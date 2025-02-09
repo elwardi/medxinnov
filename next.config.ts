08c82c904be923b67b90138d8d3b49f9a39e6fab
elwardi <youssefxp07@gmail.com>
2025-02-09 17:02:43 +0100

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  output: "export",
-  basePath: "/medxinnov",
-  assetPrefix: "/medxinnov/",
+  output: 'export', // Génère un site statique
   images: {
-    domains: ['localhost'], // Ajouter les domaines utilisés
+    unoptimized: true, // Désactive l'optimisation d'images
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/medxinnov' : '', // Remplacez par le nom de votre repo
 };
 
-export default nextConfig;
+module.exports = nextConfig;
